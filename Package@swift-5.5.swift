17559ec578bef82060a9ece99eed60aa635e70a2
MarcoDotIO <Marcus.Arnett10@gmail.com>
2022-12-16 15:31:46 -0500

    Implement different version numbers down to Swift 5.5 (iOS 13+ / other device ranges compatibility)

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,53 @@
+//  swift-tools-version: 5.5
+//
+//  Package@swift-5.5.swift
+//  OpenAIKit
+//
+//  Copyright (c) 2022 MarcoDotIO
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenAIKit",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    products: [
+        .library(
+            name: "OpenAIKit",
+            targets: ["OpenAIKit"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "OpenAIKit",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "OpenAIKitTests",
+            dependencies: ["OpenAIKit"],
+            path: "Tests",
+            resources: [.process("Resources")]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
